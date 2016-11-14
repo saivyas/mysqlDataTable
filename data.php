80e2ebc47da50861ac2b3d2dab665b2731f1620e
user <user>
2016-11-14 15:39:41 +0530

    Data Table JS

---
+++ data.php
@@ -0,0 +1,171 @@
+<?php
+// Database details
+$db_server   = 'localhost';
+$db_username = 'root';
+$db_password = '';
+$db_name     = 'arc_new1';
+
+// Get job (and id)
+$job = '';
+$id  = '';
+if (isset($_GET['job'])){
+  $job = $_GET['job'];
+  if ($job == 'get_companies' ||
+      $job == 'get_company'   ||
+      $job == 'add_company'   ||
+      $job == 'edit_company'  ||
+      $job == 'delete_company'){
+    if (isset($_GET['id'])){
+      $id = $_GET['id'];
+      if (!is_numeric($id)){
+        $id = '';
+      }
+    }
+  } else {
+    $job = '';
+  }
+}
+
+// Prepare array
+$mysql_data = array();
+
+// Valid job found
+if ($job != ''){
+  
+  // Connect to database
+  $db_connection = mysqli_connect($db_server, $db_username, $db_password, $db_name);
+  if (mysqli_connect_errno()){
+    $result  = 'error';
+    $message = 'Failed to connect to database: ' . mysqli_connect_error();
+    $job     = '';
+  }
+  
+  // Execute job
+  if ($job == 'get_companies'){
+    
+    // Get companies
+    $query = "SELECT * FROM claimnotes ORDER BY id";
+    $query = mysqli_query($db_connection, $query);
+    if (!$query){
+      $result  = 'error';
+      $message = 'query error';
+    } else {
+      $result  = 'success';
+      $message = 'query success';
+      while ($company = mysqli_fetch_array($query)){
+        $functions  = '<div class="function_buttons"><ul>';
+        $functions .= '<li class="function_edit"><a data-id="'   . $company['id'] . '" data-name="' . $company['heading'] . '"><span>Edit</span></a></li>';
+        $functions .= '<li class="function_delete"><a data-id="' . $company['id'] . '" data-name="' . $company['heading'] . '"><span>Delete</span></a></li>';
+        $functions .= '</ul></div>';
+        $mysql_data[] = array(
+          "id"          => $company['id'],
+          "heading"  => $company['heading'],
+          "claim_no"  => $company['claim_no'],
+          "description"    => $company['description'],
+          "functions"     => $functions
+        );
+      }
+    }
+    
+  } elseif ($job == 'get_company'){
+    
+    // Get company
+    if ($id == ''){
+      $result  = 'error';
+      $message = 'id missing';
+    } else {
+      $query = "SELECT * FROM claimnotes WHERE id = '" . mysqli_real_escape_string($db_connection, $id) . "'";
+      $query = mysqli_query($db_connection, $query);
+      if (!$query){
+        $result  = 'error';
+        $message = 'query error';
+      } else {
+        $result  = 'success';
+        $message = 'query success';
+        while ($company = mysqli_fetch_array($query)){
+          $mysql_data[] = array(
+          "id"        => $company['id'],
+          "heading"  => $company['heading'],
+          "claim_no"  => $company['claim_no'],
+          "description"    => $company['description']
+          );
+        }
+      }
+    }
+  
+  } elseif ($job == 'add_company'){
+    
+    // Add company
+    $heading=$_GET['heading'];
+    $claim_no=$_GET['claim_no'];
+    $description=$_GET['description'];
+    $query = "INSERT INTO claimnotes (heading,claim_no,description) values('$heading','$claim_no','$description')";
+
+    $query = mysqli_query($db_connection, $query);
+    if (!$query){
+      $result  = 'error';
+      $message = 'query error';
+    } else {
+      $result  = 'success';
+      $message = 'query success';
+    }
+  
+  } elseif ($job == 'edit_company'){
+    
+    // Edit company
+    if ($id == ''){
+      $result  = 'error';
+      $message = 'id missing';
+    } else {
+    $id=$_GET['id'];
+
+    $heading=$_GET['heading'];
+    $claim_no=$_GET['claim_no'];
+    $description=$_GET['description'];
+      $query = "UPDATE claimnotes SET heading ='$heading',claim_no='$claim_no',description='$description' WHERE id=$id";
+      $query  = mysqli_query($db_connection, $query);
+      if (!$query){
+        $result  = 'error';
+        $message = 'query error';
+      } else {
+        $result  = 'success';
+        $message = 'query success';
+      }
+    }
+    
+  } elseif ($job == 'delete_company'){
+  
+    // Delete company
+    if ($id == ''){
+      $result  = 'error';
+      $message = 'id missing';
+    } else {
+      $query = "DELETE FROM claimnotes WHERE id = '" . mysqli_real_escape_string($db_connection, $id) . "'";
+      $query = mysqli_query($db_connection, $query);
+      if (!$query){
+        $result  = 'error';
+        $message = 'query error';
+      } else {
+        $result  = 'success';
+        $message = 'query success';
+      }
+    }
+  
+  }
+  
+  // Close database connection
+  mysqli_close($db_connection);
+
+}
+
+// Prepare data
+$data = array(
+  "result"  => $result,
+  "message" => $message,
+  "data"    => $mysql_data
+);
+
+// Convert PHP array to JSON array
+$json_data = json_encode($data);
+print $json_data;
+?>
